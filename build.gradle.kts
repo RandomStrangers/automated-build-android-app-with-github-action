e333afd6e7f6d1343a6dccd07413db47466c54c8
Random Strangers <110739593+RandomStrangers@users.noreply.github.com>
2025-02-27 11:37:31 -0500

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,21 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.android.library) apply false
-    alias(libs.plugins.kotlin.android) apply false
+buildscript {
+    repositories {
+       google()
+       jcenter()
+    }
+    dependencies {
+        classpath "com.android.tools.build:gradle:3.6.4"
+    }
 }
 
-tasks.register("clean", Delete::class) {
-    delete(rootProject.layout.buildDirectory)
+allprojects {
+    repositories {
+        google()
+        jcenter()
+    }
 }
+
+tasks.register<Delete>("clean").configure {
+    delete(rootProject.buildDir)
+ }
